88112930514404608b1d6a415ae4307229d48015
Juan Melendez <juanmanual>
2019-02-20 13:03:22 -0500

    modified sql to reflect user fk in exercise log

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 rollback;
 begin transaction;
+
+set schema 'fitness-app';
 /*Begin Dropping Tables*/
 	/*Level 2 Tables*/
 		drop table if exists exerciselog;
@@ -55,7 +57,8 @@ begin transaction;
 			note              text,
 			occourred         date    not null,
 			constraint pk_exerciselog primary key (exerciselogid),
-			constraint fk_exercise foreign key (exerciseid) references exercise (exerciseid)
+			constraint fk_exercise foreign key (exerciseid) references exercise (exerciseid),
+			constraint fk_user foreign key (userid) references account (userid)
 		);
 		create table exercisesubscription(
 			exerciseid        int     not null,
