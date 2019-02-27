c6a79a1e5bc784bf788c40bfa51454aa6961f965
Kyle Serrecchia <kyserrecchia@gmail.com>
2019-02-27 01:00:57 -0500

    post exercise log is good

--- FitnessApi/src/main/java/com/revature/controllers/HistoryController.java
+++ FitnessApi/src/main/java/com/revature/controllers/HistoryController.java
@@ -3,11 +3,17 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.model.AppUser;
 import com.revature.model.ExerciseLog;
 import com.revature.services.ExerciseLogService;
 
@@ -36,4 +42,15 @@ public class HistoryController {
 	public List<ExerciseLog> findAllByUserIdAndExerciseDescription(@PathVariable int id, @PathVariable String exercise) {
 		return history.findAllByUserIdAndExerciseDescription(id, exercise);
 	}
+	
+	@PostMapping
+	@ResponseStatus(code=HttpStatus.CREATED)
+	public ExerciseLog save(@RequestBody ExerciseLog exLog) {
+		ExerciseLog ex = history.save(exLog);
+		
+		return ex;
+		
+	}
+	
+	
 }
